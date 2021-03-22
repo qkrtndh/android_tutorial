2b3ef27b4b44b8ece4cb7b50848554ced53ac564
qkrtndh <qkrtndh5@naver.com>
2021-03-22 18:00:51 +0900

    내비게이션 메뉴 바 만들기/오류많음/성공확인

---
+++ Tutorial3/app/src/main/java/com/example/tutorial3/SecondLayout.java
@@ -0,0 +1,20 @@
+package com.example.tutorial3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.app.Fragment;
+
+public class SecondLayout extends Fragment {
+    View v;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.second_layout,container,false);
+        return v;
+    }
+}
