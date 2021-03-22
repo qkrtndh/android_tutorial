c2d43b88ac329f09afa51033c8c24192cf820a65
qkrtndh <qkrtndh5@naver.com>
2021-03-22 18:22:26 +0900

    리스트 뷰 꾸미기 도중 중단/ 버전차이

---
+++ Tutorial4/app/src/main/java/com/example/tutorial4/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.tutorial4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String[] items = {"망고쥬스","토마토쥬스","포도쥬스"};
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items);
+        ListView listView = (ListView)findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = String.valueOf(parent.getItemAtPosition(position));
+                Toast.makeText(MainActivity.this,item,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
